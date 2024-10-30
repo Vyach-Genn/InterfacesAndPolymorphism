009cbac5fe0658a7750e6b6968a7d3ae04527761
vachg <vachgen@yandex.ru>
2024-10-30 15:12:55 +0300

    ООП Task 2.2

--- src/Bicycle.java
+++ src/Bicycle.java
@@ -4,6 +4,7 @@ public class Bicycle extends Transport {
         super(modelName, wheelsCount);
     }
 
+    @Override
     public void service(ServiceStation station) {
         System.out.println("Обслуживаем " + getModelName());
         for (int i = 0; i < getWheelsCount(); i++) {
--- src/Truck.java
+++ src/Truck.java
@@ -4,6 +4,7 @@ public class Truck extends Transport {
         super(modelName, wheelsCount);
     }
 
+    @Override
     public void service(ServiceStation station) {
         System.out.println("Обслуживаем " + getModelName());
         for (int i = 0; i < getWheelsCount(); i++) {
